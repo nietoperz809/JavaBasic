ffa7d82fbbc5cc3730e36c9e497845f65e489893
Administrator <pbull809@gmail.com>
2022-05-29 23:50:45 +0200

    + jarfile

--- src/basic_1/PrintItem.java
+++ src/basic_1/PrintItem.java
@@ -21,8 +21,7 @@ import misc.Misc;
 
 import java.io.PrintStream;
 
-public class PrintItem
-{
+public class PrintItem {
     public final static int EXPRESSION = 0;
     private final static int STRING_VARIABLE = 1;
     private final static int STRING_CONSTANT = 2;
@@ -32,8 +31,7 @@ public class PrintItem
     private final int type;
     private final Object thing;
 
-    public PrintItem(int t, Object o)
-    {
+    public PrintItem(int t, Object o) {
         type = t;
         thing = o;
     }
@@ -41,42 +39,37 @@ public class PrintItem
     private static final String S_TAB = "\t";
     private static final String S_SEMI = "";
 
-    public String value (Program pgm, int c) throws BASICRuntimeError
-    {
-        switch (type)
-        {
+    public String value(Program pgm, int c) throws BASICRuntimeError {
+        switch (type) {
             case EXPRESSION:
                 Expression e = (Expression) thing;
-                if (thing instanceof BooleanExpression)
-                {
+                if (thing instanceof BooleanExpression) {
                     double zz = ((Expression) thing).value(pgm);
-                    return (zz == 1) ? "TRUE" : "FALSE";
+                    return (zz == 1) ? "0" : "1";
                 }
-                if (e.isString())
-                {
+                if (e.isString()) {
                     return e.stringValue(pgm, c);
                 }
                 return (Misc.df.format(e.value(pgm)));  // <---
-            
+
             case STRING_VARIABLE:
                 return (pgm.getString((Variable) thing));
-            
+
             case STRING_CONSTANT:
                 return ((String) thing);
-            
+
             case TAB:
                 return (S_TAB);
-            
+
             case SEMI:
                 return (S_SEMI);
-            
+
             default:
                 return "BOGUS PRINTITEM";
         }
     }
 
-    public boolean needCR ()
-    {
+    public boolean needCR() {
         return ((type != TAB) && (type != SEMI));
     }
 
